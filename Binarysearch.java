e564270698dc97e9db10169aed1f5ba902af57e0
Abhinai Sai Putta <70501952+abhinaisai2002@users.noreply.github.com>
2020-10-20 14:50:18 +0530

    Update Binarysearch.java

--- Binarysearch.java
+++ Binarysearch.java
@@ -3,10 +3,9 @@ import java.util.Scanner;
 public class Binarysearch {
 	public static void main(String[] args) {
 		Scanner input = new Scanner(System.in);
-		int[] a = {5,7,43,12,56,434,54,2,8};
+		int[] a = {1,2,3,4,5,6,7,8,9};
 		System.out.println("Enter the element to be searched");
 		int number = input.nextInt();
-		a.sort();
 		int pos = binarySearch(a,number,0,8);
 		if(pos==-1)
 			System.out.println("Element is not found");
